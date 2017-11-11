72fcbee3f3d8d2f726e9d49bc9f3b305e6dbd174
leavinth <32339002+leavinth@users.noreply.github.com>
2017-11-11 02:01:34 -0800

    metadata and other stuff

--- javascript/footer.js
+++ javascript/footer.js
@@ -1,1 +1,1 @@
-document.body.innerHTML += "<footer></footer>";
+document.body.innerHTML += "<footer><p><a href="http://jigsaw.w3.org/css-validator/check/referer"><img style="border:0;width:88px;height:31px" src="http://jigsaw.w3.org/css-validator/images/vcss-blue" alt="Valid CSS!" /> </a></p></footer>";
