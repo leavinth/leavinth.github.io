dafbd6e31921dc2d01614245f67369e068b4d1d6
leavinth <32339002+leavinth@users.noreply.github.com>
2017-11-10 19:53:50 -0800

    header2

--- javascript/header.js
+++ javascript/header.js
@@ -1,2 +1,2 @@
 var title = document.title;
-document.body.innerHTML += "<header> <h1>Levi Evans</h1><h2>"+title+"</h2> <nav><a href='https://leavinth.github.io/'>Home</a> • <a href='https://leavinth.github.io/sites/portfolio/'>Portfolio</a> • <a href='https://leavinth.github.io/sites/about'>About me</a> • <a href='https://leavinth.github.io/sites/vocab'>Vocab</a></nav></header>";
+document.body.innerHTML += "<header> <h1>Levi Evans</h1><h2>"+title+"</h2> <nav><a href='https://leavinth.github.io/'>Home</a> • <a href='https://leavinth.github.io/site/portfolio/'>Portfolio</a> • <a href='https://leavinth.github.io/site/about'>About me</a> • <a href='https://leavinth.github.io/site/vocab'>Vocab</a></nav></header>";
